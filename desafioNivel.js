66d95f1bb850e79467a1a06f46b55b771e7fcb94
Leticia Rezende <leticia.rezende2010@outlook.com>
2023-11-02 15:18:08 -0300

    Desafio Concluido

---
+++ desafioNivel.js
@@ -0,0 +1,23 @@
+var nomeHeroi = "Shadow";
+var xpHeroi = 4000;
+var nivel;
+
+if (xpHeroi <= 1000) {
+    nivel = "Ferro";
+} else if (xpHeroi <= 2000) {
+    nivel = "Bronze";
+} else if (xpHeroi <= 5000) {
+    nivel = "Prata";
+} else if (xpHeroi <= 7000) {
+    nivel = "Ouro";
+} else if (xpHeroi <= 8000) {
+    nivel = "Platina";
+} else if (xpHeroi <= 9000) {
+    nivel = "Ascendente";
+} else if (xpHeroi <= 10000) {
+    nivel = "Imortal";
+} else {
+    nivel = "Radiante";
+}
+    
+console.log(`O herói de nome ${nomeHeroi} está no nivel de ${nivel}`)
